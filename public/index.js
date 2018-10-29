e835d19c72f21d03024805f9fcbb21bd6a54df1c
Maddie Becker <madeleine_becker@brown.edu>
2018-10-29 10:53:17 -0400

    Bring in web files

---
+++ public/index.js
@@ -0,0 +1,399 @@
+$(document).ready(function(){
+
+			var map, districtLayers, boundlayer;
+			// create a zoom variable, change it when new state is selected, do not change
+			// when new toggle is selected
+
+			setupmap('RI');// default state
+
+			///////////////////
+			//  STATE LIST   //
+			///////////////////
+			$.get('/states', function(data, status){
+				makeStateList(JSON.parse(data));
+			});
+
+			// Dynamically loads the list of states
+			function makeStateList(states){
+				for (var i in states){
+					var para = document.createElement("p");
+					// if state has only one district, indicate
+					if (states[i]['single_district'] == true) {
+						$(para).html(states[i]["name"] + "*");
+					} else {
+						$(para).html(states[i]["name"]);
+					}
+					$(para).prop("code", i);
+					$('#state-list').append(para);
+				}
+			}
+
+			// Functionality for searching thru states
+			$('#state-search').on('keyup search', function(e){
+				var letters = $('#state-search').val();
+				var states = $('#state-list').children();
+
+				for (var i in states){
+					var state = states[i].innerHTML;
+					console.log(states[i].innerHTML);
+					if (!state.startsWith(letters)){
+						$(states[i]).css('display','none');
+					}
+					else {
+						$(states[i]).css('display','block');
+					}
+
+				}
+
+			});
+
+
+			///////////////////////////
+			//FORM/EVENT HANDLERS ///////////
+			///////////////////////////
+
+			$('#detail-option').on('change', toggleDetail);
+			$('#bound-option').on('change', toggleBorder);
+
+
+			// adds or removes census block detailing on boundaries
+			function toggleDetail(event){
+				showLoadAnimation();
+
+				var detail_option = $('#detail-option').prop('checked');
+				console.log(detail_option);
+				var state = $('#state-input').val();
+
+				$.get('/state/'+state+'/'+detail_option,  function(res){
+					
+					drawDistricts(res); // redraws districts, which causes refresh
+					// add parameter that is toggle, T or F
+					hideLoadAnimation();
+
+				});
+				event.preventDefault();
+			}
+
+			function toggleBorder(event){
+				showLoadAnimation();
+
+				var border_option = $('#bound-option').prop('checked');
+
+				strokestyle = boundlayer.getStyle().getStroke();
+
+				if (border_option) {
+					strokestyle.setWidth(3);
+					strokestyle.setColor('black');
+
+				}
+				else {
+					strokestyle.setWidth(0);
+					strokestyle.setColor([0,0,0,0]);
+				}
+
+				boundlayer.getSource().refresh();// updates layers with style changes
+				hideLoadAnimation();
+
+				event.preventDefault();
+			}
+
+
+			$(document).on('click', 'p', function(){
+				var code = $(this).prop("code");
+
+				$('#state-input').val(code);
+				showLoadAnimation();
+				panToState(code);
+				hideLoadAnimation();
+			});
+
+
+			///////////////
+			//DRAWING MAP//
+			///////////////
+
+			// Pans to clicked state
+			function panToState(state){
+				var detail_option = $('#detail-option').prop('checked');
+				$.get('/state/'+state+'/'+detail_option, function(data, status){
+					drawDistricts(data);
+				})
+			}
+
+			// Redraws district boundaries at center of state
+			function drawDistricts(data){
+				var json = JSON.parse(data);
+				var stateData = json.geometry;
+				var districts = json.polygons;
+				var blocks = json.blocks;
+				var zoom = json.zoom;
+
+				map.getView().setCenter(ol.proj.fromLonLat(stateData.center));
+				map.removeLayer(boundlayer);
+				for (var i in districtLayers){ // global var
+					map.removeLayer(districtLayers[i]);
+				}
+				var newlayers = formLayers(stateData.type, stateData.coordinates, stateData.center, districts, blocks);
+
+				for (var j in newlayers){
+					map.addLayer(newlayers[j]);
+					console.log(map.getLayers());
+				}
+				map.getView().setZoom(zoom);
+				console.log(status);
+			}
+
+			// Initial drawing of map
+			function setupmap(state){
+				$.get('/state/'+state+'/true', function(data, status){
+
+					var json = JSON.parse(data);
+
+					var stateData = json.geometry;
+					var districts = json.polygons;
+					var zoom = json.zoom;
+					var blocks = json.blocks;
+
+					console.log(districts);
+
+					createmap(stateData.type, stateData.coordinates, stateData.center, zoom, districts, blocks);
+				})
+			}
+
+
+			// Forms the layers and their geometries but doesn't draw layers. 'blocks' is census block boundaries
+			function formLayers(type, coords, center, districts, blocks) {
+				var bound_geometry;
+				var boundfeature = new ol.Feature({});
+
+				if (type == 'Polygon'){
+					bound_geometry = new ol.geom.Polygon(coords);
+				}
+
+				else if (type == 'MultiPolygon'){
+					bound_geometry = new ol.geom.MultiPolygon(coords);
+				}
+
+				bound_geometry.transform('EPSG:4326', 'EPSG:3857');
+				boundfeature.setGeometry(bound_geometry);
+
+				districtLayers = drawDistrictLayers(districts, bound_geometry, blocks);
+
+				boundlayer = new ol.layer.Vector({
+				    source: new ol.source.Vector({
+				        features: [boundfeature]
+				    }),
+				    style: new ol.style.Style({
+				    	stroke: new ol.style.Stroke({
+				    		width: 3,
+				    		color: 'black'
+				    	}),
+				    	fill: new ol.style.Fill({
+				    		color: 'white'
+				    	})
+				    }),
+				    opacity: 0.5
+				})
+
+				return (districtLayers.concat([boundlayer]));
+			}
+
+			// Returns the ol map object... only on page reload. 'blocks' is polygons of census block boundaries
+			function createmap(type, coords, center, zoom, districts, blocks) {
+				var layers = formLayers(type, coords, center, districts, blocks);
+
+				var osm = new ol.layer.Tile({
+					source: new ol.source.OSM(),
+					opacity: 0.75
+				})
+
+				map = new ol.Map({
+					target: 'map',
+		        	layers: [osm].concat(layers),
+		        	view: new ol.View({
+		        		center: ol.proj.fromLonLat(center),
+		          		zoom: zoom
+		        	})
+		      	});
+			}
+
+			// Forms vector drawings of district layers, doesn't add to map yet
+			// change coords to be district
+			function drawDistrictLayers(coords, geometry, blocks){
+				var layers = [];
+				var colors = [];
+				// look for if RI already has colors loaded in
+				// if not, create them in a loop here
+
+				// district vectors
+				for (var i in coords){
+
+					var geom;
+					if (dimension(coords[i]) == 2) {
+						var geom = new ol.geom.Polygon([coords[i]]);
+					}
+					else {
+						var geom = new ol.geom.MultiPolygon([coords[i]]);
+					}
+
+					geom.transform('EPSG:4326', 'EPSG:3857');
+
+					color = getRandomColor().toString();
+					colors.push(color);
+
+					var vector = new ol.layer.Vector({
+				    	source: new ol.source.Vector({
+				        	features: [new ol.Feature({
+				        		geometry: geom
+				        	})]
+				    	}),
+				    	style: new ol.style.Style({
+
+				    		fill: new ol.style.Fill({
+				    			color: color
+				    		})
+				    	}),
+				    	opacity: 0.5
+					});
+
+					clipPolygon(vector, geometry);
+
+					layers.push(vector);
+				}
+
+				// census block vectors to clip district vectors
+
+				for (var j in Object.keys(blocks)){
+					console.log(blocks[j]);
+
+					var blocks_geom = new ol.geom.MultiPolygon([blocks[j]]);
+					blocks_geom.transform('EPSG:4326', 'EPSG:3857');
+
+					// This is to correct an alpha problem when overlaying census blocks over districts. we clip the district with the block clipVector using xor 
+					// for globalCompositeOperation, then draw another layer colorVector with the actual block. Inefficient but must be done to achieve desired result.
+					var clipVector = new ol.layer.Vector({
+			    	source: new ol.source.Vector({
+			        	features: [new ol.Feature({
+			        		geometry: blocks_geom
+			        	})]
+				    }),
+				   	style: new ol.style.Style({
+				   		fill: new ol.style.Fill({
+				   			color: colors[j]
+				   		})
+				   	}),
+					   	opacity: 1
+					});
+
+					var colorVector = new ol.layer.Vector({
+			    	source: new ol.source.Vector({
+			        	features: [new ol.Feature({
+			        		geometry: blocks_geom
+			        	})]
+				    }),
+				   	style: new ol.style.Style({
+				   		fill: new ol.style.Fill({
+				   			color: colors[j]
+				   		})
+				   	}),
+					   	opacity: 0.5
+					});
+
+					clipVector.on('precompose', function(event){
+						var ctx = event.context;
+						ctx.save();
+
+						ctx.globalCompositeOperation = 'xor';
+
+					});
+
+					clipVector.on('postcompose', function(event){
+						var ctx = event.context;
+						ctx.globalCompositeOperation = 'source-over';
+
+						ctx.restore();
+
+					});
+
+					// https://gist.github.com/elemoine/b95420de2db3707f2e89
+					// clipPolygon(vector, geometry);
+
+					layers.push(clipVector);
+					layers.push(colorVector);
+				}
+				return layers;
+			}
+
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+////////////////////////////      HELPERS      ////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+			function showLoadAnimation(){
+				$('#loading-gif').css('display', 'block');
+
+				var center_coords = map.getView().getCenter();
+				var loading = new ol.Overlay({
+  					element: document.getElementById('loading-gif'),
+  					position: center_coords,
+  					positioning: 'center-center'
+				});
+				// loading.setPosition(center_coords);
+				map.addOverlay(loading);
+				map.renderSync();
+			};
+
+			function hideLoadAnimation(){
+				$('#loading-gif').css('display', 'none');
+
+			}
+
+			// clips polygon vector layer (vector) to stay within the outline of the state (geometry)
+			function clipPolygon(vector, geometry){
+				// https://gist.github.com/elemoine/b95420de2db3707f2e89
+					vector.on('precompose', function(event) {
+				        var ctx = event.context;
+          				var vecCtx = event.vectorContext;
+				        ctx.save();
+
+				        var fillStyle = new ol.style.Style({fill: new ol.style.Fill({color: [0, 0, 0, 0]})});
+
+				        vecCtx.setStyle(fillStyle);
+				        vecCtx.drawGeometry(geometry);
+				        
+				        ctx.clip();
+				        
+				      });
+
+				    vector.on('postcompose', function(event) {
+				        var ctx = event.context;
+				        ctx.restore();
+				      });
+			}
+
+
+			function dimension(list){
+				console.log(list[0][0]);
+				console.log(typeof(list[0][0]));
+				if (typeof(list[0][0]) == "number") {
+					return 2
+				}
+				else {
+					return 3
+				}
+			}
+
+			// https://stackoverflow.com/questions/1484506/random-color-generator
+			function getRandomColor() {
+  				var letters = '0123456789ABCDEF';
+  				var color = '#';
+  				for (var i = 0; i < 6; i++) {
+    				color += letters[Math.floor(Math.random() * 16)];
+  				}
+  				color += ''
+  				return color;
+			}
+		   
+		});
---
+++ server.js
@@ -0,0 +1,303 @@
+/////   SET UP EXPRESS   //////
+var express = require('express');
+var app = express();
+var http = require('http');
+
+// ASYNC
+// var request = require('request');
+var async = require('async');
+const fetch = require("node-fetch");
+
+// For config files
+var fs = require('fs');
+var path = require('path');
+
+// For executing python script
+var spawnSync = require("child_process").spawnSync;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+var engines = require('consolidate');
+app.engine('html', engines.hogan); // tell Express to run .html files through Hogan
+app.set('views', __dirname); // tell Express where to find templates, in this case the '/templates' directory
+app.set('view engine', 'html'); // register .html extension as template engine so we can render .html pages 
+
+app.use(express.static(__dirname + '/public')); // client-side js & css files
+
+var stateCodes = {
+			    "AL": {
+				    	"name": "Alabama",
+				    	"single_district": false
+			    	},
+			    "AK": {
+			    		"name": "Alaska", 
+			    		"single_district": true
+			    	},
+			    "AZ": {
+			    		"name": "Arizona", 
+			    		"single_district": false
+			    	},
+			    "AR": {
+			    		"name": "Arkansas", 
+			    		"single_district": false
+			    	},
+			    "CA": {
+			    		"name": "California", 
+			    		"single_district": false
+			    	},
+			    "CO": {
+			    		"name": "Colorado", 
+			    		"single_district": false
+			    	},
+			    "CT": {
+			    		"name": "Connecticut", 
+			    		"single_district": false
+			    	},
+			    "DE": {
+			    		"name": "Delaware", 
+			    		"single_district": true
+			    	},
+			    "DC": {
+			    		"name": "District Of Columbia", 
+			    		"single_district": true
+			    	},
+			    "FL": {
+			    		"name": "Florida", 
+			    		"single_district": false
+			    	},
+			    "GA": {
+			    		"name": "Georgia", 
+			    		"single_district": false
+			    	},
+			    "HI": {
+			    		"name": "Hawaii", 
+			    		"single_district": false
+			    	},
+			    "ID": {
+			    		"name": "Idaho", 
+			    		"single_district": false
+			    	},
+			    "IL": {
+			    		"name": "Illinois", 
+			    		"single_district": false
+			    	},
+			    "IN": {
+			    		"name": "Indiana", 
+			    		"single_district": false
+			    	},
+			    "IA": {
+			    	"name": "Iowa", 
+			    		"single_district": false
+			    	},
+			    "KS": {
+			    		"name": "Kansas", 
+			    		"single_district": false
+			    	},
+			    "KY": {
+			    		"name": "Kentucky", 
+			    		"single_district": false
+			    	},
+			    "LA": {
+			    		"name": "Louisiana", 
+			    		"single_district": false
+			    	},
+			    "ME": {
+			    		"name": "Maine", 
+			    		"single_district": false
+			    	},
+			    "MD": {
+			    		"name": "Maryland", 
+			    		"single_district": false
+			    	},
+			    "MA": {
+			    		"name": "Massachusetts", 
+			    		"single_district": false
+			    	},
+			    "MI": {
+			    		"name": "Michigan", 
+			    		"single_district": false
+			    	},
+			    "MN": {
+			    		"name": "Minnesota", 
+			    		"single_district": false
+			    	},
+			    "MS": {
+			    		"name": "Mississippi", 
+			    		"single_district": true
+			    	},
+			    "MO": {
+			    		"name": "Missouri", 
+			    		"single_district": false
+			    	},
+			    "MT": {
+			    		"name": "Montana", 
+			    		"single_district": true
+			    	},
+			    "NE": {
+			    		"name": "Nebraska", 
+			    		"single_district": false
+			    	},
+			    "NV": {
+			    		"name": "Nevada", 
+			    		"single_district": false
+			    	},
+			    "NH": {
+			    		"name": "New Hampshire", 
+			    		"single_district": false
+			    	},
+			    "NJ": {
+			    		"name": "New Jersey", 
+			    		"single_district": true
+			    	},
+			    "NM": {
+			    		"name": "New Mexico", 
+			    		"single_district": false
+			    	},
+			    "NY": {
+			    		"name": "New York", 
+			    		"single_district": false
+			    	},
+			    "NC": {
+			    		"name": "North Carolina", 
+			    		"single_district": false
+			    	},
+			    "ND": {
+			    		"name": "North Dakota", 
+			    		"single_district": true 
+			    	},
+			    "OH": {
+			    		"name": "Ohio", 
+			    		"single_district": false 
+			    	},
+			    "OK": {
+			    		"name": "Oklahoma", 
+			    		"single_district": false 
+			    	},
+			    "OR": {
+			    		"name": "Oregon", 
+			    		"single_district": false 
+			    	},
+			    "PA": {
+			    		"name": "Pennsylvania", 
+			    		"single_district": false 
+			    	},
+			    "RI": {
+			    		"name": "Rhode Island", 
+			    		"single_district": false 
+			    	},
+			    "SC": {
+			    		"name": "South Carolina", 
+			    		"single_district": false 
+			    	},
+			    "SD": {
+			    		"name": "South Dakota", 
+			    		"single_district": true 
+			    	},
+			    "TN": {
+			    		"name": "Tennessee", 
+			    		"single_district": false 
+			    	},
+			    "TX": {
+			    		"name": "Texas", 
+			    		"single_district": false 
+			    	},
+			    "UT": {
+			    		"name": "Utah", 
+			    		"single_district": false 
+			    	},
+			    "VT": {
+			    		"name": "Vermont", 
+			    		"single_district": true 
+			    	},
+			    "VA": {
+			    		"name": "Virginia", 
+			    		"single_district": false 
+			    	},
+			    "WA": {
+			    		"name": "Washington", 
+			    		"single_district": false 
+			    	},
+			    "WV": {
+			    		"name": "West Virginia", 
+			    		"single_district": true 
+			    	},
+			    "WI": {
+			    		"name": "Wisconsin", 
+			    		"single_district": false 
+			    	},
+			    "WY": {
+			    		"name": "Wyoming", 
+			    		"single_district": true 
+			    	}
+				}
+
+/////   ROUTING   //////
+
+// main page of the site
+app.get('/', function(req, res){
+	res.render('index.html');
+});
+
+// There is a global variable above called stateCodes, which we send along as a response to a get request in index.js in makeStateList(). 
+// This helps form the list of states on the left side of the UI
+app.get('/states', async function(req, res){
+	res.send(JSON.stringify(stateCodes));
+});
+
+// This is to get all data for drawing the map. Takes in a :name parameter (the state code) and a :detailopt (whether or not we should 
+// send along the census block detail)
+app.get('/state/:name/:detailopt', function(req, res){
+	var statejson = fs.readFileSync('../district-web-data/states.json'); // contains information about each state, including state boundary and census area. take a look in the file for more info
+
+	var state = JSON.parse(statejson).features.find(state => (state.properties.NAME == stateCodes[req.params.name]["name"]));
+
+	var	stateGeom = state.geometry; // get the geometry/coordinates corresponding to the state in question
+	var zoom = (11.5 - Math.log10(state.properties.CENSUSAREA)); // a cute little equation i experimentally came up with to calculate a reasonable zoom setting for open layers
+	console.log(zoom);
+
+	var detail_opt = req.params.detailopt;
+	console.log(detail_opt);
+	var census_blocks, poly_file;
+
+	// we're about to read in the district_polygons, i.e. power cells, pre-computed for state and stored in web_data
+	fs.readFile('../district-web-data/web_data/district_polygons/polygons_'+req.params.name, 'utf8', function(err, data){
+
+		if (err){
+			console.log('There does not already exist a file with the polygons for this districting of this state');
+			console.log(err);
+
+			// pass in empty data so that server doesn't crash, will just show outline of state
+			var json = {"polygons":[], "geometry":stateGeom, "blocks":[], "zoom":zoom}
+
+		}
+		else {
+			if (detail_opt == 'true'){
+				raw_blocks = fs.readFileSync('../district-web-data/web_data/census_polygons/boundary_blocks_'+req.params.name+'.json');
+				census_blocks = JSON.parse(raw_blocks);
+
+			}
+			else {
+				census_blocks = [];
+			}
+			// string processing because arrays in python are different than arrays in js.
+			var polygons = JSON.parse(data.replace(/\(/g, '\[').replace(/\)/g, '\]'));
+			var json = {"polygons":polygons, "geometry":stateGeom, "blocks": census_blocks, "zoom":zoom};
+		}
+
+		// send it back to the functions that did a get request to this route in index.js
+		res.send(JSON.stringify(json));
+	});
+});
+
+// SERVER SET UP
+
+app.listen(8080, function(){
+	console.log('– Server listening on port 8080');
+});
+
+process.on('SIGINT', function(){
+	console.log('\nI\'m closing');
+	process.exit(0);
+});
